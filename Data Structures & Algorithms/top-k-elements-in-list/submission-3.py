6cd44da2565cfe18f730434c2e6f8a5f91b7ab71
Krati Choudhary <117533993+kratichoudhary25@users.noreply.github.com>
2026-08-01 15:05:09 -0400

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        c = Counter(nums)
+        return [item[0] for item in c.most_common(k)]
